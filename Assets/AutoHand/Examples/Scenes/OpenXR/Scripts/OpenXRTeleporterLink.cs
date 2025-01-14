d826084c460cb7812269bdc3b465eb74c4fccdcf
DebbyX3 <deborah.pintani@univr.it>
2025-01-14 15:18:38 +0100

    Edit OpenXRTeleporterLink.cs to support canceling a teleport action

--- Assets/AutoHand/Examples/Scenes/OpenXR/Scripts/OpenXRTeleporterLink.cs
+++ Assets/AutoHand/Examples/Scenes/OpenXR/Scripts/OpenXRTeleporterLink.cs
@@ -5,36 +5,49 @@ using UnityEngine.InputSystem;
 using UnityEngine.XR;
 
 namespace Autohand.Demo{
-    public class OpenXRTeleporterLink : MonoBehaviour{
+    public class OpenXRTeleporterLink : MonoBehaviour {
         public Teleporter hand;
         public InputActionProperty startTeleportAction;
         public InputActionProperty finishTeleportAction;
-        
+        public InputActionProperty cancelTeleportAction;
+
         bool teleporting = false;
 
         void OnEnable() {
-            if(startTeleportAction.action != null) startTeleportAction.action.Enable();
+            if (startTeleportAction.action != null) startTeleportAction.action.Enable();
             if (startTeleportAction.action != null) startTeleportAction.action.performed += StartTeleportAction;
             if (finishTeleportAction.action != null) finishTeleportAction.action.Enable();
             if (finishTeleportAction.action != null) finishTeleportAction.action.performed += FinishTeleportAction;
+            if (cancelTeleportAction.action != null) cancelTeleportAction.action.Enable();
+            if (cancelTeleportAction.action != null) cancelTeleportAction.action.performed += CancelTeleportAction;
         }
 
-        void OnDisable() { 
+        void OnDisable() {
             if (startTeleportAction.action != null) startTeleportAction.action.performed -= StartTeleportAction;
             if (finishTeleportAction.action != null) finishTeleportAction.action.performed -= FinishTeleportAction;
+            if (cancelTeleportAction.action != null) cancelTeleportAction.action.performed -= CancelTeleportAction;
         }
 
 
         void StartTeleportAction(InputAction.CallbackContext a) {
-            if(!teleporting){
+            if (!teleporting) {
                 hand.StartTeleport();
                 teleporting = true;
             }
         }
 
         void FinishTeleportAction(InputAction.CallbackContext a) {
-            if(teleporting){
+            if (teleporting) {
                 hand.Teleport();
+                teleporting = false;
+            }
+        }
+
+        // Custom code for Farmacologia App
+        // Cancel teleport when corresponding button is pressed
+        void CancelTeleportAction(InputAction.CallbackContext a) {
+            if(teleporting){
+                hand.CancelTeleport();
                 teleporting = false;
             }
         }
